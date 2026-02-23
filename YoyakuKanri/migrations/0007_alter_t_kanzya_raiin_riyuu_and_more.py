9993cfb90c0b5aeb0357bb951483e328bd51f043
Takumi157239 <f-t-work@outlook.jp>
2026-02-23 16:56:01 +0900

    JsonFieldをテキストフィールドに変更

--- YoyakuKanri/migrations/0007_alter_t_kanzya_raiin_riyuu_and_more.py
+++ YoyakuKanri/migrations/0007_alter_t_kanzya_raiin_riyuu_and_more.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='t_kanzya',
             name='RAIIN_RIYUU',
-            field=models.JSONField(blank=True, default=dict),
+            field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
             model_name='t_kanzya',
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='t_kanzya',
             name='ZYOUHOU_MOTO',
-            field=models.JSONField(blank=True, default=dict),
+            field=models.TextField(blank=True, null=True),
         ),
     ]
